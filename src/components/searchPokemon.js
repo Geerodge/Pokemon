9e0c16bab250d85b0f59434741c07558c9b9d053
George McEntegart <geerodge@gmail.com>
2021-06-20 19:56:47 +0100

    Update searchPokemon.js
    Functionality to search for a pokemon via the form input added.

--- src/components/searchPokemon.js
+++ src/components/searchPokemon.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import fetch from 'node-fetch';
 
-// Pokemon Endpoints
-const baseEndpoint = "https://pokeapi.co/api/v2/pokemon";
-
-// Function to fetch Pokemon by name
-async function displayPokemon(name) {
-  const response = await fetch(`${baseEndpoint}/${name}`);
-  const data = await response.json();
-  console.log(data);
-}
-
-// Error handler
-function handleError(err) {
-  console.log(`Bugger! Something went wrong: ${err}`);
-  console.log(err);
-}
-
-displayPokemon('pikachu').catch(handleError);
-
 export default function PokemonSearch() {
+  // Pokemon Endpoints
+  const baseEndpoint = "https://pokeapi.co/api/v2/pokemon";
+
+  // Function to fetch Pokemon by name
+  async function displayPokemon(name) {
+    const response = await fetch(`${baseEndpoint}/${name}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  // Error handler
+  function handleError(err) {
+    console.log(`Bugger! Something went wrong: ${err}`);
+    console.log(err);
+  }
+
+  // Tracks the value of the form input
+  const [name, setPokemon] = useState("pikachu");
+
+  function handleChange(e) {
+    setPokemon(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    displayPokemon(name).catch(handleError);
+  }
+
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
           <input 
               type="text"
               placeholder="Search for Pokémon..."
+              onChange={handleChange}
           />
       </form>
 )}
