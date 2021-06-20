5387b2797e68a3cd597dbc78de53af997beedd53
George McEntegart <geerodge@gmail.com>
2021-06-20 15:37:12 +0100

    House keeping

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Pokemon - Gotta catch 'em all!`,
+    description: `Search details about different Pokemon with help from the Pokeapi.`,
     author: `@gatsbyjs`,
   },
   plugins: [
